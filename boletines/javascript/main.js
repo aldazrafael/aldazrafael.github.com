5ffa622f056fd2e77942074d81fcb407c34d6195
aldazrafael <68709285+aldazrafael@users.noreply.github.com>
2020-08-22 18:44:33 -0500

    Update main.js

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -45,7 +45,7 @@ links.forEach(function(link){
     content.classList.add("animate__animated");
 
     setTimeout(function(){
-    location.href = "/boletines";
+    location.href = "../boletines";
     },600);
 
     return false;
